98053efc3e0a6a18bdc065ca571d0780b28db7bd
nullTheCoder <stepan.gaidukevich@gmail.com>
2020-11-02 19:46:56 +0200

    yes

--- src/net/dreamingworld/gameplay/manacraft/Manacraft.java
+++ src/net/dreamingworld/gameplay/manacraft/Manacraft.java
@@ -5,6 +5,7 @@ import net.dreamingworld.gameplay.manacraft.blocks.BasicManaGenerator;
 import net.dreamingworld.gameplay.manacraft.items.HotPeddle;
 import net.dreamingworld.gameplay.manacraft.items.HotStaff;
 import net.dreamingworld.gameplay.manacraft.items.HotSword;
+import net.dreamingworld.gameplay.manacraft.items.ManaCore;
 import org.bukkit.Bukkit;
 
 public class Manacraft {
@@ -16,7 +17,9 @@ public class Manacraft {
         // Items
         new HotPeddle();
         new HotSword();
+        new ManaCore();
 
+        //register events
         Bukkit.getPluginManager().registerEvents(new HotStaff(), DreamingWorld.getInstance());
     }
 }
