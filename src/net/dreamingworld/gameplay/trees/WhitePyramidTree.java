46ba95867a6d57b7e0ab09dbd8787767d5da2df8
nullTheCoder <stepan.gaidukevich@gmail.com>
2020-11-13 18:27:07 +0200

    better new tree

--- src/net/dreamingworld/gameplay/trees/WhitePyramidTree.java
+++ src/net/dreamingworld/gameplay/trees/WhitePyramidTree.java
@@ -3,6 +3,7 @@ package net.dreamingworld.gameplay.trees;
 import net.dreamingworld.DreamingWorld;
 import net.dreamingworld.core.structures.EasyBuilder;
 import net.dreamingworld.core.structures.Structure;
+import net.minecraft.server.v1_8_R3.MathHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.WorldType;
@@ -22,7 +23,7 @@ public class WhitePyramidTree extends Structure {
     @Override
     public void generate(EasyBuilder builder) {
         Bukkit.getScheduler().runTaskAsynchronously(DreamingWorld.getInstance(), () -> {
-            int height = ThreadLocalRandom.current().nextInt(4, 20 + 1);
+            int height = ThreadLocalRandom.current().nextInt(4, 12 + 1);
             int finalHeight = 0;
 
             for (int i = 0; i <= height / 2 ; i++) {
@@ -35,17 +36,20 @@ public class WhitePyramidTree extends Structure {
                 builder.buildCustomBlock(0, i - 3, 0, "white_log");
                 builder.buildCustomBlock(0, i - 4, 0, "white_log");
                 for (int i2 = 1; i2 < height - (i/4); i2++) {
-                    builder.buildCustomBlock(i2, i2 + (height / 4) + i, 0, "white_log");
-                    builder.buildCustomBlock(i2, i2 + 1 + (height / 4) + i, 0, "holy_leaf");
 
-                    builder.buildCustomBlock(-i2, i2 + (height / 4) + i, 0, "white_log");
-                    builder.buildCustomBlock(-i2, i2 + 1 + (height / 4) + i, 0, "holy_leaf");
+                    int i2_ = MathHelper.clamp(i2, 0, height/2);
 
-                    builder.buildCustomBlock(0, i2 + (height / 4) + i, i2, "white_log");
-                    builder.buildCustomBlock(0, i2 + 1 + (height / 4) + i, i2, "holy_leaf");
+                    builder.buildCustomBlock(i2_, i2_ + (height / 4) + i, 0, "white_log");
+                    builder.buildCustomBlock(i2_, i2_ + 1 + (height / 4) + i, 0, "holy_leaf");
 
-                    builder.buildCustomBlock(0, i2 + (height / 4) + i, -i2, "white_log");
-                    builder.buildCustomBlock(0, i2 + 1 + (height / 4) + i, -i2, "holy_leaf");
+                    builder.buildCustomBlock(-i2_, i2_ + (height / 4) + i, 0, "white_log");
+                    builder.buildCustomBlock(-i2_, i2_ + 1 + (height / 4) + i, 0, "holy_leaf");
+
+                    builder.buildCustomBlock(0, i2_ + (height / 4) + i, i2_, "white_log");
+                    builder.buildCustomBlock(0, i2_ + 1 + (height / 4) + i, i2_, "holy_leaf");
+
+                    builder.buildCustomBlock(0, i2_ + (height / 4) + i, -i2_, "white_log");
+                    builder.buildCustomBlock(0, i2_ + 1 + (height / 4) + i, -i2_, "holy_leaf");
                 }
                 finalHeight = i-1;
             }
